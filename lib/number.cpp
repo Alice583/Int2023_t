d79d0243d3e24c5f56042750bd12499c47bfea11
Alice <jr.alchemy@gmail.com>
2023-11-06 00:15:37 +0300

    lab work is ready

--- lib/number.cpp
+++ lib/number.cpp
@@ -1,5 +1,7 @@
 #include "number.h"
 
+char numberr[10] = {'0','1', '2', '3', '4', '5', '6', '7', '8', '9', };
+
 
 int2023_t from_int(int32_t i) {
     int2023_t int2023;
@@ -262,8 +264,88 @@ int2023_t operator*(const int2023_t &lhs, const int2023_t &rhs) {
 }
 
 int2023_t operator/(const int2023_t &lhs, const int2023_t &rhs) {
-    return int2023_t();
-//    int2023_t in;
+    int2023_t int2023 = from_int(0);
+    int index_left_num;
+    int index_right_num;
+    for (index_left_num = 1; index_left_num <= 252; index_left_num++) {
+        if (lhs.number[index_left_num] != '0') {
+            break;
+        }
+    }
+    for (index_right_num = 1; index_right_num <= 252; index_right_num++) {
+        if (rhs.number[index_right_num] != '0') {
+            break;
+        }
+    }
+    if (lhs == from_int(0) || rhs == from_int(0)){
+        return int2023;
+    }
+
+    if (lhs.number[index_left_num] < rhs.number[index_right_num]) {
+        if (index_right_num <= index_left_num) {
+            return int2023;
+        }
+    }
+    if (rhs == from_int(1)) {
+        return lhs;
+    }
+    int flag_rhs_digit;
+    if (rhs.number[index_right_num] <= lhs.number[index_left_num]) {
+        flag_rhs_digit = 0;
+    } else {
+        flag_rhs_digit = 1;
+    }
+    int res_digit = 0;
+    int position = index_left_num + (253 - index_right_num) + flag_rhs_digit;
+    int result_length = 254 - position;
+    char result[result_length];
+    int2023_t remainder = from_int(0);
+    for (int i = 0; i < ((253 - index_right_num) + flag_rhs_digit); i++) {
+        remainder.number[index_right_num + i - flag_rhs_digit] = lhs.number[index_left_num + i];
+    }
+    int2023_t divider = rhs;
+    divider.number[0] = '+';
+    for (int i = 0; i < result_length; i++) {
+        int2023_t temp = remainder - divider;
+        char temp_sign = temp.number[0];
+        for (int j = 0; j < 253; j++) {
+            temp.number[j] = remainder.number[j];
+        }
+        int num_pos = 0;
+        while (remainder.number[0] == '+') {
+            remainder = remainder - divider;
+            num_pos++;
+        }
+        remainder = temp - (divider * from_int(num_pos - 1));
+        if (temp_sign == '+') {
+            result[res_digit] = numberr[num_pos - 1];
+        } else {
+            result[res_digit] = '0';
+        }
+        res_digit++;
+
+        for (int j = 2; j < 253; j++) {
+            remainder.number[j - 1] = remainder.number[j];
+        }
+        remainder.number[252] = lhs.number[position];
+        position++;
+    }
+    for (int i = 0; i < result_length; i++) {
+        int2023.number[254 - result_length + i - 1] = result[i];
+    }
+
+//    if (lhs.number[0] != rhs.number[0]) {
+//        int2023.number[0] = '-';
+//    } else {
+//        int2023.number[0] = '+';
+//    }
+    if ((CheckSignPlus(lhs.number[0]) && CheckSignPlus(rhs.number[0])) ||
+        (!CheckSignPlus(lhs.number[0]) && !CheckSignPlus(rhs.number[0]))) {
+        int2023.number[0] = '+';
+    } else {
+        int2023.number[0] = '-';
+    }
+    return int2023;
 }
 
 bool operator==(const int2023_t &lhs, const int2023_t &rhs) {
